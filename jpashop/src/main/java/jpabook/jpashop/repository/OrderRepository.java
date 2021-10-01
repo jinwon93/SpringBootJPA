9cc7f60811748e769abee3e20f2199770032f9aa
jinwon93 <kjw8208@nate.com>
2021-10-01 23:22:59 +0900

    Member

--- jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -21,7 +21,7 @@ public class OrderRepository {
         return em.find(Order.class , id);
     }
 
-    public List<Order> findAll(OrderSearch orderSearch){
-
-    }
+//    public List<Order> findAll(OrderSearch orderSearch){
+//
+//    }
 }
