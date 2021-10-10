534f6df28101b5340aae0b6b8a371626add6e96c
jinwon93 <kjw8208@nate.com>
2021-10-10 20:03:11 +0900

    오더apiDto

---
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -0,0 +1,43 @@
+package jpabook.jpashop.api;
+
+
+
+import jpabook.jpashop.domain.Order;
+
+import jpabook.jpashop.dto.SimpleOrderDto;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+
+    private  final OrderRepository orderRepository;
+
+
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> orderV2(){
+        //ORDER
+        // N+!1  -->  1 + 회원N(2) + 배송N (2)
+        // fetch join으로 성능개선 (개조)
+        //지연로딩은 영속성 컨텍스트에서 조회하므로  이미 조회된 경우 쿼리를 생락한다
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+
+        return result;
+    }
+
+
+}
