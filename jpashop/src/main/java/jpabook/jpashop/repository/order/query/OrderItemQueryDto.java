bdbadce185b76ebbbb979efd100e9109e69e646e
jinwon93 <kjw8208@nate.com>
2021-10-16 21:05:17 +0900

    orderQuery

---
+++ jpashop/src/main/java/jpabook/jpashop/repository/order/query/OrderItemQueryDto.java
@@ -0,0 +1,22 @@
+package jpabook.jpashop.repository.order.query;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+
+@Data
+public class OrderItemQueryDto {
+
+    @JsonIgnore
+    private  Long orderId;
+    private String itemName;
+    private int OrderPrice;
+    private int count;
+
+    public OrderItemQueryDto(Long orderId, String itemName, int orderPrice, int count) {
+        this.orderId = orderId;
+        this.itemName = itemName;
+        this.OrderPrice = orderPrice;
+        this.count = count;
+    }
+}
