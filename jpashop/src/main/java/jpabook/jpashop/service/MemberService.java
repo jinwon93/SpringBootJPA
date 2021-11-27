8d83e4baf68292928f29e08b24f090ef28f3c3ab
jinwon93 <kjw8208@nate.com>
2021-11-27 15:01:14 +0900

    j1

--- jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
+++ jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -32,7 +32,7 @@ public class MemberService {
 
     private void validateDuplicateMember(Member member) {
         Optional<Member> findMembers = memberRepository.findByName(member.getName());
-        if (!findMembers.isEmpty()){
+        if (!findMembers.isPresent()){
             throw new IllegalStateException("이미 존재하는 회원입니다");
         }
     }
