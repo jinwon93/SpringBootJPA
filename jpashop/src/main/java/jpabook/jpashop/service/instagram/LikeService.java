323ba52a279323de342346c202055fc356fa2fe8
jinwon93 <kjw8208@nate.com>
2021-12-26 20:00:30 +0900

    20212126

--- jpashop/src/main/java/jpabook/jpashop/service/instagram/LikeService.java
+++ jpashop/src/main/java/jpabook/jpashop/service/instagram/LikeService.java
@@ -1,9 +1,12 @@
 package jpabook.jpashop.service.instagram;
 
 
+import jpabook.jpashop.core.handler.CustomApiException;
 import jpabook.jpashop.repository.instagram.LikeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -15,7 +18,12 @@ public class LikeService {
         try {
             likeRepository.likes(postId ,  sessionId);
         } catch (Exception e){
-            throw new Exception("ddd");
+            throw new CustomApiException("이미 좋아요 하였습니다");
         }
+    }
+
+    @Transactional
+    public void  unLikes(long postId ,  long sessionId){
+        likeRepository.unLikes(postId ,  sessionId);
     }
 }
