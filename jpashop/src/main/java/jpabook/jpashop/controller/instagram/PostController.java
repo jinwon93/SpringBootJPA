19f8b2d8f7d60c99a20e3fdc772cf88c1488f62f
jinwon93 <kjw8208@nate.com>
2022-01-05 23:11:56 +0900

    20220105

--- jpashop/src/main/java/jpabook/jpashop/controller/instagram/PostController.java
+++ jpashop/src/main/java/jpabook/jpashop/controller/instagram/PostController.java
@@ -1,10 +1,18 @@
 package jpabook.jpashop.controller.instagram;
 
 
+import jpabook.jpashop.core.auth.PrincipalDetails;
+import jpabook.jpashop.core.handler.CustomApiException;
+import jpabook.jpashop.dto.instagram.post.PostUploadDto;
 import jpabook.jpashop.service.instagram.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequiredArgsConstructor
@@ -12,8 +20,23 @@ public class PostController {
 
     private final PostService postService;
 
+
+    //post 업로드 화면으로 이동
     @GetMapping("/post/upload")
     public String upload(){
         return "post/upload";
+    }
+
+    //post 업로드 후 프로필 화면으로 이동
+    @PostMapping("post")
+    public String uploadPost(PostUploadDto postUploadDto , @RequestParam("uploadImgUrl")MultipartFile multipartFile,
+                             RedirectAttributes redirectAttributes , @AuthenticationPrincipal PrincipalDetails principalDetails){
+
+        if (multipartFile.isEmpty()){
+            throw  new CustomApiException("이미지가 첨부되지 않았습니다");
+        }
+        postService.save(postUploadDto , multipartFile , principalDetails);
+        redirectAttributes.addAttribute("id" , principalDetails.getUser().getId());
+        return "redirect:/user/profile";
     }
 }
