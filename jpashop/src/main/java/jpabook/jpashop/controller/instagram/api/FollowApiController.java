6f40f184e7d8e698fce01364fffbdd44eff43b26
jinwon93 <kjw8208@nate.com>
2022-01-17 21:06:27 +0900

    20220117

--- jpashop/src/main/java/jpabook/jpashop/controller/instagram/api/FollowApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/controller/instagram/api/FollowApiController.java
@@ -1,8 +1,14 @@
 package jpabook.jpashop.controller.instagram.api;
 
 
+import jpabook.jpashop.core.auth.PrincipalDetails;
 import jpabook.jpashop.service.instagram.FollowService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +18,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class FollowApiController {
 
     private final FollowService followService;
+
+
+    @PostMapping("/follow/{toUserId}")
+    public ResponseEntity<?> followUser(@PathVariable long toUserId  , @AuthenticationPrincipal PrincipalDetails principalDetails){
+        followService.follow(principalDetails.getUser().getId() , toUserId);
+        return new ResponseEntity<>("팔로우 성공" , HttpStatus.OK);
+    }
+
+    public ResponseEntity<?> unFollowUser(@PathVariable long toUserId , @AuthenticationPrincipal PrincipalDetails principalDetails){
+        followService.unFollow(principalDetails.getUser().getId() , toUserId);
+        return new ResponseEntity<>("팔로우 최소 성공 " , HttpStatus.OK);
+    }
 }
