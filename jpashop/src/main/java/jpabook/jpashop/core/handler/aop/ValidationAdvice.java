0dea49ab13e56ffd3aede8ded74e0c9178c2beb8
jinwon93 <kjw8208@nate.com>
2022-01-31 23:35:52 +0900

    20220131

---
+++ jpashop/src/main/java/jpabook/jpashop/core/handler/aop/ValidationAdvice.java
@@ -0,0 +1,50 @@
+package jpabook.jpashop.core.handler.aop;
+
+
+import jpabook.jpashop.core.handler.ex.CustomValidationApiException;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+@Aspect
+public class ValidationAdvice {
+
+
+    @Around("execution(* moon.)")
+    public Object apiAdvice(ProceedingJoinPoint proceedingJoinPoint) throws Throwable{
+        Object[] args = proceedingJoinPoint.getArgs();
+        for (Object  arg : args){
+            BindingResult bindingResult = (BindingResult) arg;
+            if (bindingResult.hasErrors()){
+                Map<String , String> errorMap = new HashMap<>();
+                for (FieldError error  : bindingResult.getFieldErrors()){
+                    errorMap.put(error.getField() , error.getDefaultMessage());
+                }
+                throw new CustomValidationApiException("유효성 검사 실패" , errorMap);
+            }
+        }
+        return proceedingJoinPoint.proceed();
+    }
+
+
+    public Object advice(ProceedingJoinPoint proceedingJoinPoint) throws  Throwable{
+        Object[] args = proceedingJoinPoint.getArgs();
+        for (Object arg : args){
+            if (arg instanceof BindingResult){
+                BindingResult bindingResult = (BindingResult) arg;
+                if (bindingResult.hasErrors()){
+                    Map<String , String> errorMap = new HashMap<>();
+                }
+            }
+        }
+        return null;
+    }
+
+}
