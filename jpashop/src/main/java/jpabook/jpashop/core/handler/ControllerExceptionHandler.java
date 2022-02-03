1366dfb689f426bde93fa4d6497a0685db3d23a5
jinwon93 <kjw8208@nate.com>
2022-02-03 22:12:59 +0900

    20220203

---
+++ jpashop/src/main/java/jpabook/jpashop/core/handler/ControllerExceptionHandler.java
@@ -0,0 +1,28 @@
+package jpabook.jpashop.core.handler;
+
+
+import jpabook.jpashop.core.handler.ex.CustomVaildationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+
+    public String validationException(CustomVaildationException e){
+        if (e.getErrorMap() == null){
+            return null;
+        }
+        return null;
+    }
+
+
+    @ExceptionHandler(CustomVaildationException.class)
+    public ResponseEntity<?> validationApiException(CustomVaildationException e){
+        return new ResponseEntity<>(e.getMessage() , HttpStatus.BAD_REQUEST);
+    }
+}
